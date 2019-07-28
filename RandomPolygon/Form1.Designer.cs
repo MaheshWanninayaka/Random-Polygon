35a48cef75442c9ed0251ee72a09d2762020c150
maheshwanninayaka <mahesharavinda40@gmail.com>
2019-07-28 08:26:35 +0530

    first commit

---
+++ RandomPolygon/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace RandomPolygon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Vertices = new System.Windows.Forms.TextBox();
+            this.button_Genarate = new System.Windows.Forms.Button();
+            this.button_Clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(364, 415);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(386, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Number of Vertices";
+            // 
+            // textBox_Vertices
+            // 
+            this.textBox_Vertices.Location = new System.Drawing.Point(487, 12);
+            this.textBox_Vertices.Name = "textBox_Vertices";
+            this.textBox_Vertices.Size = new System.Drawing.Size(55, 20);
+            this.textBox_Vertices.TabIndex = 2;
+            // 
+            // button_Genarate
+            // 
+            this.button_Genarate.Location = new System.Drawing.Point(386, 50);
+            this.button_Genarate.Name = "button_Genarate";
+            this.button_Genarate.Size = new System.Drawing.Size(75, 23);
+            this.button_Genarate.TabIndex = 3;
+            this.button_Genarate.Text = "Genarate";
+            this.button_Genarate.UseVisualStyleBackColor = true;
+            this.button_Genarate.Click += new System.EventHandler(this.button_Genarate_Click);
+            // 
+            // button_Clear
+            // 
+            this.button_Clear.Location = new System.Drawing.Point(467, 50);
+            this.button_Clear.Name = "button_Clear";
+            this.button_Clear.Size = new System.Drawing.Size(75, 23);
+            this.button_Clear.TabIndex = 4;
+            this.button_Clear.Text = "Clear";
+            this.button_Clear.UseVisualStyleBackColor = true;
+            this.button_Clear.Click += new System.EventHandler(this.button_Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 439);
+            this.Controls.Add(this.button_Clear);
+            this.Controls.Add(this.button_Genarate);
+            this.Controls.Add(this.textBox_Vertices);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Random Polygon Genarator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_Vertices;
+        private System.Windows.Forms.Button button_Genarate;
+        private System.Windows.Forms.Button button_Clear;
+    }
+}
+
---
+++ RandomPolygon/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomPolygon
+{
+    public partial class Form1 : Form
+    {
+
+        int[] x = new int[100];
+        int[] y = new int[100];
+        int points;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DisplayLine(int x1, int y1, int x2, int y2)
+        {
+            Graphics g = panel1.CreateGraphics();
+            g.DrawLine(Pens.Black, x1, panel1.Height - y1, x2, panel1.Height - y2);
+        }
+
+        private void DeleteLine(int x1, int y1, int x2, int y2)
+        {
+            Graphics g = panel1.CreateGraphics();
+            g.DrawLine(Pens.White, x1, panel1.Height - y1, x2, panel1.Height - y2);
+        }
+
+        private void DrawPolygon()
+        {
+            int i, j;
+            for (i = 0; i < points; i++)
+            {
+                j = (i + 1) % points;
+                DisplayLine(x[i], y[i], x[j], y[j]);
+            }
+        }
+
+        private void DeletePolygon()
+        {
+            int i, j;
+            for (i = 0; i < points; i++)
+            {
+                j = (i + 1) % points;
+                DeleteLine(x[i], y[i], x[j], y[j]);
+            }
+        }
+
+        private long t(int x1, int y1, int x2, int y2, int x3, int y3)
+        {
+            return x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2);
+        }
+
+        private Boolean isTwoSides(int i, int j)
+        {
+            int k;
+            bool n, p;
+            long value;
+            n = false;
+            p = false;
+
+            for (k = 0; k < points; k++)
+            {
+                value = t(x[i], y[i], x[j], y[j], x[k], y[k]);
+                if (value == 0)
+                {
+                    if ((i != k) && (j != k))
+                    {
+                        return true;
+                    }
+                }
+                else if (value < 0)
+                {
+                    n = true;
+                }
+                else
+                {
+                    p = true;
+                }
+            }
+
+            if ((n == true) && (p == false))
+            {
+                return false;
+            }
+            else if ((n == false) && (p == true))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+
+        private void button_Genarate_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            Boolean found;
+            Boolean convex;
+            int i, j;
+
+            points = int.Parse(textBox_Vertices.Text);
+
+            found = false;
+            while (!found)
+            {
+                convex = true;
+
+                for (i = 0; i < points; i++)
+                {
+                    x[i] = r.Next(0, panel1.Width);
+                    y[i] = r.Next(0, panel1.Height);
+                }
+
+                for (i = 0; i < points; i++)
+                {
+                    j = (i + 1) % points;
+                    if (isTwoSides(i, j))
+                    {
+                        convex = false;
+                        break;
+                    }
+                }
+
+                if (convex)
+                {
+                    DrawPolygon();
+                    found = true;
+                }
+            }
+        }
+
+        private void button_Clear_Click(object sender, EventArgs e)
+        {
+            DeletePolygon();
+        }
+    }
+}
